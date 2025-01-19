9c614866ddaac4c0199e7ff55b4838abdb246ca0
Sarvagya Kumar <111130903+sarvagyakrcs@users.noreply.github.com>
2025-01-19 17:39:11 +0530

    feat(page.tsx): updated resume link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -221,7 +221,7 @@ function Resume() {
           <Role key={roleIndex} role={role} />
         ))}
       </ol>
-      <Button href="https://drive.google.com/file/d/1xfUGgNl55d6EzBaMXBMXePfo2M40DwzA/view" variant="secondary" className="group mt-6 w-full">
+      <Button href="https://drive.google.com/file/d/1wRSo6La4t6T4F1IwKfEywPbyRbrAHpy3/view?usp=sharing" variant="secondary" className="group mt-6 w-full">
         Download CV
         <ArrowDownIcon className="h-4 w-4 stroke-zinc-400 transition group-active:stroke-zinc-600 dark:group-hover:stroke-zinc-50 dark:group-active:stroke-zinc-50" />
       </Button>
